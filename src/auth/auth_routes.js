7c32b65c1bf646e518fda56f56590e3265fec06d
Belfor Acuña <131080733+belfor-acuna@users.noreply.github.com>
2024-07-17 21:32:08 -0400

    feat: :sparkles: Middlewares build
    New middlewares for auth token and user role verification

--- src/auth/auth_routes.js
+++ src/auth/auth_routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import {getMe, login,register} from './auth_controller.js'
+import authMiddleware from './middleware/auth_middleware.js';
+import roleMiddleware from '../user/middleware/role_middleware.js';
 
 router.post('/login', login);
 router.post('/register',  register);
-router.get('/me',getMe)
+router.get('/me',authMiddleware,getMe)
 export default router;
